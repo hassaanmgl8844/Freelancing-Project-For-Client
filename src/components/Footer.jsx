0de52c7190371aae669a30c9a5db48f924e67f9d
hassaanmgl8844 <hsnprog8844@gmail.com>
2024-10-23 21:44:50 +0500

    Updates Everything We Need

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,6 @@
+import { FaFacebook, FaInstagram, FaTwitter, FaPinterest, FaYoutube } from "react-icons/fa6";
+import { FaCcVisa , FaCcMastercard , FaPaypal } from "react-icons/fa";
+import { SiAmericanexpress } from "react-icons/si";
 
 function Footer() {
   return (
@@ -30,19 +33,19 @@ function Footer() {
         <div>
           <h3 className="font-bold mb-2">NEWSLETTER</h3>
           <form>
-            <input 
-              type="email" 
-              placeholder="Enter your email address" 
-              className="px-4 py-2 rounded border border-gray-400 mb-4 w-full" 
+            <input
+              type="email"
+              placeholder="Enter your email address"
+              className="px-4 py-2 rounded border border-gray-400 mb-4 w-full"
             />
             <button className="bg-red-500 text-white px-4 py-2 rounded w-full">Join</button>
           </form>
           <div className="flex justify-center mt-4 space-x-4">
-            <a href="#"><img src="/path/to/facebook-icon.png" alt="Facebook" /></a>
-            <a href="#"><img src="/path/to/instagram-icon.png" alt="Instagram" /></a>
-            <a href="#"><img src="/path/to/twitter-icon.png" alt="Twitter" /></a>
-            <a href="#"><img src="/path/to/pinterest-icon.png" alt="Pinterest" /></a>
-            <a href="#"><img src="/path/to/youtube-icon.png" alt="YouTube" /></a>
+            <a href="#"><FaFacebook /></a>
+            <a href="#"><FaInstagram /></a>
+            <a href="#"><FaTwitter /></a>
+            <a href="#"><FaPinterest /></a>
+            <a href="#"><FaYoutube /></a>
           </div>
         </div>
       </div>
@@ -51,10 +54,10 @@ function Footer() {
           <p>&copy; 2023 VenoArt. All rights reserved.</p>
         </div>
         <div className="flex space-x-4">
-          <img src="/path/to/visa-icon.png" alt="Visa" />
-          <img src="/path/to/mastercard-icon.png" alt="Mastercard" />
-          <img src="/path/to/amex-icon.png" alt="American Express" />
-          <img src="/path/to/paypal-icon.png" alt="Paypal" />
+          <FaCcVisa />
+          <FaCcMastercard />
+          <SiAmericanexpress />
+          <FaPaypal />
         </div>
       </div>
     </footer>
